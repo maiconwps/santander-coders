bd62d861ba20795f5a31193fa5b6c8e6b22f7371
Maicon <maiconobmepufpa@gmail.com>
2021-11-03 15:54:30 -0400

    desafio promisses

---
+++ assicrono/main.js
@@ -0,0 +1,31 @@
+const btnGenerateList = document.getElementById("btn-generate-list")
+const spanList = document.getElementById("list")
+
+generate_random_number = limit => Math.round(Math.random() * limit)
+generate_random_numbers = (length, max) => Array(length).fill(0).map(() =>generate_random_number(max))
+
+createPromises = (time, length) => new Promise((resolve) => setTimeout(resolve, time, generate_random_numbers(length, 100)))
+concatResultValues = (arrayValues, result) => arrayValues.concat(result.value)
+
+createItemList = text => {
+    const li = document.createElement("li")
+    li.innerText = text
+    li.classList.add("btn")
+    return li
+}
+addItemsInSpanList = items => items.forEach(item => spanList.appendChild(createItemList(item))) 
+
+btnGenerateList.onclick = () => {
+    const timePromisses = [
+        generate_random_number(5),
+        generate_random_number(5),
+        generate_random_number(5)]
+    spanList.innerHTML = ""
+    
+    const promises = timePromisses.map((time) => createPromises(time, generate_random_number(10)))
+    Promise.allSettled(promises).then((results) => {
+        const values = results.reduce(concatResultValues,[])
+        console.log(values)
+        addItemsInSpanList(values)
+    })
+}
