7b63e6c3fb591cf45d44e727fa6d995180771a09
Maicon <maiconobmepufpa@gmail.com>
2021-11-04 15:42:08 -0400

    add ordenaçao do resultado

--- assicrono/main.js
+++ assicrono/main.js
@@ -1,4 +1,5 @@
 const btnGenerateList = document.getElementById("btn-generate-list")
+const btnSortList = document.querySelector("button[name=order]")
 const spanList = document.getElementById("list")
 
 generate_random_number = limit => Math.round(Math.random() * limit)
@@ -15,6 +16,39 @@ createItemList = text => {
 }
 addItemsInSpanList = items => items.forEach(item => spanList.appendChild(createItemList(item))) 
 
+function get_sort_permutation(array, predicate){
+    let permuationArray = []
+
+    for(const index in array){
+        if(permuationArray.length === 0){
+           permuationArray[0] = index
+        }
+        else{
+            let i = 0
+
+            while(predicate(array[index], array[permuationArray[i]]) > 0 && i < index){
+                i++
+            }
+
+            permuationArray.splice(i, 0, index)
+        }
+    }
+
+    return permuationArray
+}
+
+function transform(element, tranformation){
+    element.style.transform = tranformation
+}
+function translateAnimation(element, values){
+    const x = values.x
+    const y = values.y
+
+    for(let i=1 ; i<=100; i++){
+        setTimeout(transform, 15*i, element, `translate(${x/100 * i}px, ${y/100 * i}px)`)
+    }
+}
+
 btnGenerateList.onclick = () => {
     const timePromisses = [
         generate_random_number(5),
@@ -27,4 +61,22 @@ btnGenerateList.onclick = () => {
         const values = results.reduce(concatResultValues,[])
         addItemsInSpanList(values)
     })
+    btnSortList.classList.remove("--is-disable")
+}
+
+
+btnSortList.onclick = () => {
+    const numberItems =  Array.from(document.querySelectorAll("li.btn"))
+    const randomNumbers = numberItems.map(numberItem => Number(numberItem.innerText))
+
+    const sortPermutationNumbers = get_sort_permutation(randomNumbers, (a, b) => a-b)
+
+    numberItems.map((element, index) => {
+        const numberItem = numberItems[sortPermutationNumbers[index]]
+
+        const nextReact = element.getBoundingClientRect()
+        const currentReact = numberItem.getBoundingClientRect()
+        translateAnimation(numberItem, {y: (nextReact.y - currentReact.y), x: (nextReact.x - currentReact.x)})
+    })
+    btnSortList.classList.add("--is-disable")
 }
